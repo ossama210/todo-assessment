a8d2f6e64429a67a59591f9d60dc4d998dd97def
Ossama Mulhim <ossamamulhim21@gmail.com>
2025-03-04 22:35:34 +0200

    add users table and configure one to many relationship

---
+++ backend/prisma/migrations/20250304202808_user_table_with_relation/migration.sql
@@ -0,0 +1,18 @@
+-- AlterTable
+ALTER TABLE "Todo" ADD COLUMN     "userId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
