d47d91c8681cc29f94ecaec9a9a4f6e9d0fd6a73
Ossama Mulhim <ossamamulhim21@gmail.com>
2025-03-05 13:03:19 +0200

    hash passwords on create

--- backend/src/resources/user/user.service.ts
+++ backend/src/resources/user/user.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/services/prisma/prisma.service';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
-  create(createUserDto: Prisma.UserCreateInput) {
+  async create(createUserDto: Prisma.UserCreateInput) {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+    createUserDto.password = hashedPassword;
     return this.prisma.user.create({ data: createUserDto });
   }
 
